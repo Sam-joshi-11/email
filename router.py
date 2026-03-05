b525bc9051659112d2891555358397797a96e672
Sam-joshi-11 <samarthjoshi89517@gmail.com>
2026-03-05 19:32:09 +0530

    fix email environment variables

--- router.py
+++ router.py
@@ -1,11 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from model import ContactForm
 import smtplib
 import os
 from email.mime.text import MIMEText
-from dotenv import load_dotenv
-
-load_dotenv()
 
 router = APIRouter()
 
@@ -16,6 +13,9 @@ EMAIL_TO = os.getenv("EMAIL_TO")
 
 @router.post("/send-message")
 def send_message(data: ContactForm):
+
+    if not EMAIL_USER or not EMAIL_PASS or not EMAIL_TO:
+        raise HTTPException(status_code=500, detail="Email configuration missing")
 
     body = f"""
 New Contact Form Message
@@ -28,17 +28,20 @@ Message:
 {data.message}
 """
 
-    msg = MIMEText(body)
+    try:
+        msg = MIMEText(body)
+        msg["Subject"] = "New Website Contact Form"
+        msg["From"] = EMAIL_USER
+        msg["To"] = EMAIL_TO
 
-    msg["Subject"] = "New Website Contact Message"
-    msg["From"] = EMAIL_USER
-    msg["To"] = EMAIL_TO
-    msg["Reply-To"] = data.email
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.starttls()
+        server.login(EMAIL_USER, EMAIL_PASS)
 
-    server = smtplib.SMTP("smtp.gmail.com", 587)
-    server.starttls()
-    server.login(EMAIL_USER, EMAIL_PASS)
-    server.sendmail(EMAIL_USER, EMAIL_TO, msg.as_string())
-    server.quit()
+        server.sendmail(EMAIL_USER, EMAIL_TO, msg.as_string())
+        server.quit()
 
-    return {"status": "Message sent successfully"}
+        return {"status": "Email sent successfully"}
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
